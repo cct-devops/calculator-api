2e1077742e7c3f1d207154ade3e2758a8453b4e5
David Gonzalez <dagonza1983@gmail.com>
2023-07-11 19:50:30 +0100

    Added multiplication

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const fastify = Fastify({
   logger: true
 })
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-})
-
-fastify.get('/test', function (request, reply) {
-    reply.send({hello: 'cct'});
-})
-
 fastify.get('/sum', function (request, reply) {
     const a = parseInt(request.query['a']);
     const b = parseInt(request.query['b']);
     reply.send({result: a + b});
 })
+
+fastify.get('/mul', function (request, reply) {
+    const a = parseInt(request.query['a']);
+    const b = parseInt(request.query['b']);
+    reply.send({result: a * b});
+})
+
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
     if (err) {
